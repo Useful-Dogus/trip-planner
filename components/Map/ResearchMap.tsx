36486f860a76eb7389528da539f5b4301ce6bcc1
chanhee-park <chanhee13p@gmail.com>
2026-03-25 16:34:59 +0900

    feat: 지도 타일을 CartoDB Positron으로 교체

--- components/Map/ResearchMap.tsx
+++ components/Map/ResearchMap.tsx
@@ -46,8 +46,8 @@ export default function ResearchMap({ items }: { items: TripItem[] }) {
       className="touch-none"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       />
       {mapItems.map(item => (
         <Marker
