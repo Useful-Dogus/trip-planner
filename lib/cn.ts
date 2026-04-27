6e2e3db9e74b291758c2122e68e063bf6bb6353b
chanhee-park <chanhee13p@gmail.com>
2026-04-27 17:05:38 +0900

    fix(ui): cn.ts ClassValue 타입에 bigint 추가 (TS 컴파일 오류 해결)

--- lib/cn.ts
+++ lib/cn.ts
@@ -3,7 +3,7 @@
  * - falsy(null/false/undefined/'') 제거 후 공백으로 join.
  * - 외부 의존성을 늘리지 않기 위해 clsx 미사용.
  */
-export type ClassValue = string | number | null | false | undefined | ClassValue[]
+export type ClassValue = string | number | bigint | null | false | undefined | ClassValue[]
 
 export function cn(...inputs: ClassValue[]): string {
   const out: string[] = []
